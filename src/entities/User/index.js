c1d655bce082ac2d71e9647b6d654cfd43bcba78
DyakovaDaria <diakovvadaria@icloud.com>
2024-04-18 20:31:44 +0300

    user ui folder changes

--- src/entities/User/index.js
+++ src/entities/User/index.js
@@ -1,5 +1,9 @@
 import reducer from './model/UserSlice';
+import UserProfile from './ui/userProfile/UserProfile';
+import UserPreview from './ui/userPreview/UserPreview';
 
 export {
-  reducer as userReducer
+  reducer as userReducer,
+  UserPreview,
+  UserProfile,
 };
