7562edc3813503bbf50bd1dc29ed3f5a2b97fd8e
DyakovaDaria <diakovvadaria@icloud.com>
2024-04-26 01:17:13 +0300

    .

--- src/features/TeacherSchedule/ui/TeacherScheduleEdit/addClassForm/AddClassForm.jsx
+++ src/features/TeacherSchedule/ui/TeacherScheduleEdit/addClassForm/AddClassForm.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { updateSchedule, toggleClassCreationModal } from "../../../model/TeacherScheduleSlice";
+import {
+  updateSchedule,
+  toggleClassCreationModal,
+} from "../../../model/TeacherScheduleSlice";
 import {
   selectClassroom,
   loadClassrooms,
@@ -10,8 +13,8 @@ import addClassFormStyles from "./AddClassForm.module.css";
 const AddClassForm = () => {
   const dispatch = useDispatch();
   const [newClass, setNewClass] = useState({
-    type: null,
-    id: null,
+    type: "individual",
+    id: Date.now,
     title: "",
     description: "",
     startTime: "",
@@ -24,53 +27,24 @@ const AddClassForm = () => {
   const classrooms = useSelector((state) => state.classroom.list);
   const [selectedClassroom, setSelectedClassroom] = useState(null);
 
-  const handleInputChange = (event) => {
-    // const { name, value } = event.target;
-    // if (name === "type") {
-    //   const optionsReset =
-    //     question.type !== value &&
-    //     (value === "short-text" || value === "extended-answer")
-    //       ? []
-    //       : question.options.map((opt) => ({ ...opt, correct: false }));
-    //   updateSchedule({ ...question, [name]: value, options: optionsReset });
-    // } else {
-    //   updateSchedule({ ...question, [name]: value });
-    // }
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewClass((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleSave = () => {
-    // if (
-    //   !question.questionText.trim() ||
-    //   question.options.some((option) => !option.text.trim())
-    // ) {
-    //   alert("Question text and all options must be filled.");
-    //   return;
-    // }
-    // if (
-    //   (question.type === "single-choice" || question.type === "mult-choice") &&
-    //   !question.options.some((option) => option.correct)
-    // ) {
-    //   alert("At least one correct answer must be selected.");
-    //   return;
-    // }
-    // dispatch(addQuestion(question));
-    // dispatch(toggleQuestionCreationModal(false));
+    console.log(newClass);
+    // dispatch(addClass({ teacherId, date, classData }));
+    dispatch(toggleClassCreationModal(false));
   };
 
   const handleClose = () => {
     dispatch(toggleClassCreationModal(false));
   };
-  const [startTime, setStartTime] = useState("10:00");
-  const [finishTime, setFinishTime] = useState("11:00"); 
 
   React.useEffect(() => {
     dispatch(loadClassrooms());
   }, [dispatch]);
-
-  const handleRadioChange = (e) => {
-    setSelectedClassroom(e.target.value);
-    dispatch(selectClassroom(e.target.value));
-  };
 
   const chooseClassStyle = (classStatus) => {
     if (classStatus === "free") {
@@ -93,8 +67,7 @@ const AddClassForm = () => {
           <div className={addClassFormStyles.startTimeContainer}>
             <label htmlFor="startTime">с</label>
             <input
-              onChange={(e) => setStartTime(e.value)}
-              value={startTime}
+              onChange={handleInputChange}
               name="startTime"
               className={addClassFormStyles.timePick}
               type="time"
@@ -103,8 +76,7 @@ const AddClassForm = () => {
           <div className={addClassFormStyles.endTimeContainer}>
             <label htmlFor="endTime">до</label>
             <input
-              onChange={(e) => setFinishTime(e.value)}
-              value={finishTime}
+              onChange={handleInputChange}
               name="endTime"
               className={addClassFormStyles.timePick}
               type="time"
@@ -120,7 +92,7 @@ const AddClassForm = () => {
                 id={classroom.id}
                 name="classroom"
                 value={classroom.id}
-                onChange={handleRadioChange}
+                onChange={handleInputChange}
                 class={addClassFormStyles.radioButton}
                 disabled={classroom.status === "occupied"}
               />
@@ -138,9 +110,17 @@ const AddClassForm = () => {
           ))}
         </div>
         <h3>Ученик</h3>
-        <input type="text" class={addClassFormStyles.studentNameInp} placeholder="Введите ФИО Ученика"/>
-        <button class={addClassFormStyles.acceptBtn}>Сохранить</button>
-        <button class={addClassFormStyles.cancelBtn} onClick={handleClose}>Отменить</button>
+        <input
+          type="text"
+          class={addClassFormStyles.studentNameInp}
+          name="student"
+          onChange={handleInputChange}
+          placeholder="Введите ФИО Ученика"
+        />
+        <button class={addClassFormStyles.acceptBtn} onClick={handleSave}>Сохранить</button>
+        <button class={addClassFormStyles.cancelBtn} onClick={handleClose}>
+          Отменить
+        </button>
       </form>
     </div>
   );
