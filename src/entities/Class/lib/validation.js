0eddd9a3c4cc1271d9dd7e7446932b6357be75e3
DyakovaDaria <diakovvadaria@icloud.com>
2024-05-15 18:45:54 +0300

    validation fix

--- src/entities/Class/lib/validation.js
+++ src/entities/Class/lib/validation.js
@@ -6,11 +6,14 @@ export const validation = (lesson) => {
   if (!lesson.startTime) {
     errors.endTime = "Выберите время окончания занятия";
   }
-  if (!lesson.endTime) {
-    errors.endTime = "Выберите время окончания занятия";
+  if (!lesson.name) {
+    errors.endTime = "Введите название занятия";
   }
   if (!lesson.classroom) {
     errors.classroom = "Выберите класс для занятия";
+  }
+  if (!lesson.teacher) {
+    errors.classroom = "Выберите преподавателя для занятия";
   }
   return errors;
 };
